1453f10e054bd82e56b20aca5572e35363698127
Max Wesley <mpsw30@gmail.com>
2017-02-09 16:45:09 -0500

    Fixed book model for serialization

--- book_api/Controllers/BooksController.cs
+++ book_api/Controllers/BooksController.cs
@@ -4,15 +4,28 @@ using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Web.Http;
+using System.Web.Script.Serialization;
 
 namespace book_api.Controllers
 {
     public class BooksController : ApiController
     {
         // GET api/values, this needs to return a json or xml of books and their descriptors for tabular data
-        public IEnumerable<string> Get()
+        public IEnumerable<Book> Get()
         {
-            return new string[] { "book1", "book2" };
+            List<Book> bookList = new List<Book>();
+            //create book objects and add to the list
+            Book kiteRunner = new Book
+            {
+                Title = "The Kite Runner",
+                Author = "Khaled H.",
+                Genre = "Realistic Fiction",
+                Publisher = "Penguin"
+            };
+            bookList.Add(kiteRunner);
+
+            //return list of books
+            return bookList;
         }
 
         // GET api/values/5, for accessing a single book? unneeded but possibly cool
@@ -20,5 +33,13 @@ namespace book_api.Controllers
         {
             return $"value: {id}";
         }
+    }
+
+    public class Book
+    {
+        public string Title { get; set; }
+        public string Author { get; set; }
+        public string Genre { get; set; }
+        public string Publisher { get; set; }
     }
 }
